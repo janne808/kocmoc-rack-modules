ae1b17dc879146b3ea882138b9af3bbd3558ea6e
Janne Heikkarainen <janne808@radiofreerobotron.net>
2019-10-17 05:41:12 +0200

    Add GPLv3 license

--- src/svf.h
+++ src/svf.h
@@ -3,20 +3,20 @@
  *
  *  All rights reserved.
  *
- *  This file is part of State Variable Filter VST plugin.
+ *  This file is part of State Variable Filter VCV Rack plugin.
  *
- *  State Variable Filter VST plugin is free software: you can redistribute it and/or modify
+ *  State Variable Filter VCV Rack plugin is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  State Variable Filter VST plugin is distributed in the hope that it will be useful,
+ *  State Variable Filter VCV Rack plugin is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with State Variable Filter VST plugin.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with State Variable Filter VCV Rack plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __dspsvfh__
