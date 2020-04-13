40b2f2bd63ac84dfea8bd598b468814d6c6c65f0
Janne Heikkarainen <janne808@radiofreerobotron.net>
2020-04-13 20:56:12 +0200

    Remove unstable integration method from module menu

--- src/fir.h
+++ src/fir.h
@@ -3,20 +3,20 @@
  *
  *  All rights reserved.
  *
- *  This file is part of State Variable Filter VCV Rack plugin.
+ *  This file is part of Finite Impulse Response Filter VCV Rack plugin.
  *
- *  State Variable Filter VCV Rack plugin is free software: you can redistribute it and/or modify
+ *  Finite Impulse Response Filter VCV Rack plugin is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  State Variable Filter VCV Rack plugin is distributed in the hope that it will be useful,
+ *  Finite Impulse Response Filter VCV Rack plugin is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with State Variable Filter VCV Rack plugin.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Finite Impulse Response Filter VCV Rack plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __dspfirh__
